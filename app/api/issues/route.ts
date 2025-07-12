88113b4bac258c6cbd8e127a6b27306774cb70ef
Mohammad Shirani <mohammad.shirani.developer@gmail.com>
2025-07-12 11:59:25 +0330

    Handel form submition

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -10,8 +10,12 @@ const createIssueSchema= z.object({
  export async function POST(request:NextRequest){
     const body=await request.json()
     const validation=createIssueSchema.safeParse(body)
+    
+    
     if(!validation.success)
         return NextResponse.json(validation.error.errors,{status:400 })
+    console.log(validation);
+    
     const newIssue= await prisma.issue.create({
         data:{title:body.title,description:body.description}
     })
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,15 +1,44 @@
 "use client";
 import { Button, TextField } from "@radix-ui/themes";
-import SimpleMDE from "react-simplemde-editor";
+
+import dynamic from "next/dynamic";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
 import "easymde/dist/easymde.min.css";
+import { useRouter } from "next/navigation";
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
   return (
-    <div className="max-w-xl space-y-3">
-      <TextField.Root placeholder="title" />
-      <SimpleMDE placeholder="Description" />
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        console.log(data);
+
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      })}
+    >
+      <TextField.Root placeholder="title" {...register("title")} />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
+
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
